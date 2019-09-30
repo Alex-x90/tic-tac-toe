b82d2b5bda4241ac05259cd57f75d866246bc035
PPS\SRosenthal6608 <FranklinGamedev@gmail.com>
2019-09-30 15:15:26 -0700

    Set up start select buttons/game start.

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 import tkinter as tk
 from tkinter import *
+from functools import partial
 
 class custButton(tk.Button):
     def __init__(self,row,col,master=None,**kwargs):
-        tk.Button.__init__(self,master,**kwargs)
+        self.var = tk.StringVar()
+        tk.Button.__init__(self,master,textvariable=self.var,**kwargs)
         self.row=row
         self.col=col
-    def press(self):
-        print(str(self.row)+" "+str(self.col))
+        self.get, self.set = self.var.get, self.var.set
+    def press(self,mode):
+        print(str(self.row)+" "+str(self.col)+" "+str(mode))
+
+def game(mode):
+    for i in range(3):
+        for j in range(3):
+            frame = Frame(root,height=128,width=128)
+            frame.pack_propagate(0)
+            frame.grid(row=i,column=j)
+            button = custButton(i,j,frame)
+            button.set("-")
+            button.configure(command=partial(button.press, mode),font=("","75"))
+            frame.grid(row=i,column=j)
+            button.pack(fill=BOTH, expand=1)
+
+def start(mode,select):
+    select.destroy()
+    game(mode)
 
 root = Tk()
 root.title("Tic Tac Toe")
 root.resizable(width=False, height=False)
 
-for i in range(3):
-    for j in range(3):
-        temp = Frame(root,height=128,width=128)
-        temp.pack_propagate(0)
-        temp.grid(row=i,column=j)
-        temp = custButton(temp,i,j,text="testing",command=temp.press)
-        temp.pack(fill=BOTH, expand=1)
+select = Frame(root)
+select.grid()
+
+label = Label(select,text="How do you want to play?")
+multiplayer = Button(select,text="1 vs 1",command=lambda: start(1,select))
+ai = Button(select,text="vs ai",command=lambda: start(0,select))
+label.grid(row=0,column=0,columnspan=2)
+multiplayer.grid(row=1,column=0)
+ai.grid(row=1,column=1)
 
 root.mainloop()
