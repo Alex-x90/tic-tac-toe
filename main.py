defe34534dd89b02f4aff0373132d4420a3f6d44
PPS\SRosenthal6608 <FranklinGamedev@gmail.com>
2019-10-08 09:46:30 -0700

    Implemented AI. AI sucks at game

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class myApp(tk.Tk):
         self.title("Tic Tac Toe")
         self.resizable(width=False, height=False)
         self.buttons = {}
-        self.turn = 0
+        self.turn = -1
 
         self.select = Frame(self)
         self.select.grid()
@@ -35,51 +35,83 @@ class myApp(tk.Tk):
             self.buttons[x].pack(fill=BOTH, expand=1)
 
     def press(self,button):
-        if self.turn:
+        if self.turn==1:
             self.buttons[button].set("O")
             self.buttons[button].configure(state=DISABLED)
         else:
             self.buttons[button].set("X")
             self.buttons[button].configure(state=DISABLED)
-        self.turn = not self.turn
-        if self.winCheck()==-1:
+        self.turn = self.turn * -1
+        temp = self.winCheck(self.genBoard())
+        if temp==-1:
             self.popupMsg("X wins")
-        if self.winCheck()==1:
+        if temp:
             self.popupMsg("O wins")
-        if self.winCheck()==0:
+        if temp==0:
             self.popupMsg("draw")
-        if self.mode==0:
-            print("test")
+        if self.mode==0 and self.turn==1:
+            self.press(self.bestMove(self.genBoard()))
 
-    def winCheck(self):
-        if(self.buttons[0].get()==self.buttons[1].get() and self.buttons[1].get()==self.buttons[2].get() and self.buttons[0].get()!="-"):
-            if self.buttons[0].get()=="O": return 1
-            else: return -1
-        if(self.buttons[3].get()==self.buttons[4].get() and self.buttons[4].get()==self.buttons[5].get() and self.buttons[3].get()!="-"):
-            if self.buttons[3].get()=="O": return 1
-            else: return -1
-        if(self.buttons[6].get()==self.buttons[7].get() and self.buttons[7].get()==self.buttons[8].get() and self.buttons[6].get()!="-"):
-            if self.buttons[6].get()=="O": return 1
-            else: return -1
-        if(self.buttons[0].get()==self.buttons[3].get() and self.buttons[3].get()==self.buttons[6].get() and self.buttons[0].get()!="-"):
-            if self.buttons[0].get()=="O": return 1
-            else: return -1
-        if(self.buttons[1].get()==self.buttons[4].get() and self.buttons[4].get()==self.buttons[7].get() and self.buttons[1].get()!="-"):
-            if self.buttons[1].get()=="O": return 1
-            else: return -1
-        if(self.buttons[2].get()==self.buttons[5].get() and self.buttons[5].get()==self.buttons[8].get() and self.buttons[2].get()!="-"):
-            if self.buttons[2].get()=="O": return 1
-            else: return -1
-        if(self.buttons[0].get()==self.buttons[4].get() and self.buttons[4].get()==self.buttons[8].get() and self.buttons[0].get()!="-"):
-            if self.buttons[0].get()=="O": return 1
-            else: return -1
-        if(self.buttons[2].get()==self.buttons[4].get() and self.buttons[4].get()==self.buttons[6].get() and self.buttons[2].get()!="-"):
-            if self.buttons[2].get()=="O": return 1
-            else: return -1
+    def bestMove(self,board):
+        move=None
+        score=float('-inf')
+        for x in self.getMoves(board):
+            tempBoard = board.copy()
+            tempBoard[x]=1
+            tempScore=self.minimax(board,self.turn)
+            if tempScore>score:
+                move=x
+                score=tempScore
+        return move
+
+    def minimax(self,board,turn):
+        if self.winCheck(board) != None:
+            return self.winCheck(board)
+
+        move = -1
+        score = -2
+
+        moves = self.getMoves(board)
+        for x in moves:
+            tempBoard = board.copy()
+            tempBoard[x]= turn
+            tempTurn = turn*-1
+            scoreMove = -self.minimax(tempBoard,tempTurn)
+            if scoreMove>score:
+                score=scoreMove
+                move = x
+
+        if move==-1:
+            return 0
+
+        return score
+
+    def genBoard(self):
+        board = [0]*9
         for x in range(9):
-            if self.buttons[x].get()=="-":
-                return
-        return 0
+            if(self.buttons[x].get()=="X"):
+                board[x]=-1
+            if(self.buttons[x].get()=="O"):
+                board[x]=1
+        return board
+
+    def winCheck(self,board):
+        if(board[0]==board[1]==board[2] and board[0]!=0):
+            return board[0]
+        if(board[3]==board[4]==board[5] and board[3]!=0):
+            return board[3]
+        if(board[6]==board[7]==board[8] and board[6]!=0):
+            return board[6]
+        if(board[0]==board[3]==board[6] and board[0]!=0):
+            return board[0]
+        if(board[1]==board[4]==board[7] and board[1]!=0):
+            return board[1]
+        if(board[2]==board[5]==board[8] and board[2]!=0):
+            return board[2]
+        if(board[0]==board[4]==board[8] and board[0]!=0):
+            return board[0]
+        if(board[2]==board[4]==board[6] and board[2]!=0):
+            return board[2]
 
     def popupMsg(self,msg):
         for x in range(9):
@@ -96,10 +128,17 @@ class myApp(tk.Tk):
 
     def newGame(self):
         self.popup.destroy()
-        self.turn = 0
+        self.turn = -1
         for x in range(9):
             self.buttons[x].set("-")
             self.buttons[x].configure(state=NORMAL)
+
+    def getMoves(self,board):
+        output = []
+        for x in range(9):
+            if board[x]==0:
+                output.append(x)
+        return output
 
 class custButton(tk.Button):
     def __init__(self,master=None,**kwargs):
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-turn = 0 #turn 0 is x(player)'s turn
+turn = 1 #turn -1 is x(player)'s turn
 
 def minimax(board,turn):
     if winCheck() != None:
@@ -7,12 +7,20 @@ def minimax(board,turn):
     move = -1
     score = -2
 
-    moves = getPossibleMoves()
+    moves = getPossibleMoves(board)
     for x in moves:
         tempBoard = board.copy()
-        tempBoard[x]=
-        tempTurn=turn
+        tempBoard[x]= turn
+        tempTurn = not turn
+        scoreMove = -self.minimax(tempBoard,tempTurn)
+        if scoreMove>score:
+            score=scoreMove
+            move = x
 
+    if move==-1:
+        return 0
 
-test = [1,0,0,0,0,0,0,0,0]
+    return score
+
+test = [-1,0,0,0,0,0,0,0,0]
 minimax(test,turn)
